ca633036b796bad1a6946ce058b7b4bbdd72e835
hanxiaojian <1095906242@qq.com>
2023-02-08 22:35:56 +0800

    创建项目，嵌套路由

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CityList from "./pages/CityList";
+
+import { Button } from "antd-mobile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/home">首页</Link>
+          </li>
+          <li>
+            <Link to="/citylist">城市选择</Link>
+          </li>
+        </ul>
+
+        <Route path="/home" component={Home}></Route>
+        <Route path="/citylist" component={CityList}></Route>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import News from "../News";
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div style={{ backgroundColor: "skyblue", padding: 10 }}>
+        首页
+        <Route path="/home/news" component={News}></Route>
+      </div>
+    );
+  }
+}
