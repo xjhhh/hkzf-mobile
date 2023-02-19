4611bf9c8310f1439872fa1cd7cbb51c9fc60916
hanxiaojian <1095906242@qq.com>
2023-02-19 17:11:47 +0800

    使用axios请求数据（轮播图）

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,33 +1,46 @@
 import React from "react";
 import { Swiper } from "antd-mobile";
-import "./index.css";
-const imgs = [
-  "AiyWuByWklrrUDlFignR",
-  "TekJlZRVCjLFexlOCuWn",
-  "IJOtIlfsYdTyaDTRVrLI",
-];
-
-const items = imgs.map((val, index) => (
-  <Swiper.Item key={index}>
-    <a
-      key={val}
-      href="http://itcast.cn"
-      style={{ display: "inline-block", width: "100%", height: 212 }}
-    >
-      <img
-        src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
-        style={{ width: "100%", verticalAlign: "top" }}
-      ></img>
-    </a>
-  </Swiper.Item>
-));
+import axios from "axios";
 
 export default class Index extends React.Component {
+  state = {
+    swipers: [],
+  };
+
+  async getSwipers() {
+    const res = await axios.get("http://localhost:8080/home/swiper");
+    this.setState({
+      swipers: res.data.body,
+    });
+  }
+  componentDidMount() {
+    this.getSwipers();
+  }
+
+  renderSwipers() {
+    if (!this.state.swipers.length) {
+      return <Swiper.Item></Swiper.Item>;
+    }
+    return this.state.swipers.map((item) => (
+      <Swiper.Item key={item.id}>
+        <a
+          key={item.id}
+          href="http://itcast.cn"
+          style={{ display: "inline-block", width: "100%", height: 212 }}
+        >
+          <img
+            src={`http://localhost:8080${item.imgSrc}`}
+            style={{ width: "100%", verticalAlign: "top" }}
+          ></img>
+        </a>
+      </Swiper.Item>
+    ));
+  }
   render() {
     return (
       <div style={{ width: "100%" }}>
         <Swiper loop autoplay autoplayInterval={5000}>
-          {items}
+          {this.renderSwipers()}
         </Swiper>
       </div>
     );
