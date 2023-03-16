72fcea09b2a5da688abeead090a2ef99aaa427b4
hanxiaojian <1095906242@qq.com>
2023-03-16 23:33:37 +0800

    城市选择列表的顶部导航栏

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import { NavBar } from "antd-mobile";
+import "./index.scss";
+const Nav = () => {
+  const navigate = useNavigate();
+  const onBack = () => {
+    navigate(-1);
+  };
+  return (
+    <NavBar
+      className="navbar"
+      backArrow={<i className="iconfont icon-back"></i>}
+      onBack={onBack}
+    >
+      城市选择
+    </NavBar>
+  );
+};
 export default class CityList extends React.Component {
   render() {
-    return <div>这是城市选择</div>;
+    return (
+      <div>
+        <Nav></Nav>
+      </div>
+    );
   }
 }
