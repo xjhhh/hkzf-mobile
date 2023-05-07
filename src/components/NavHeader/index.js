e81cd458a2272850766da83ee341857b0ab97856
hanxiaojian <1095906242@qq.com>
2023-05-07 20:26:48 +0800

    为组件添加props校验

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { NavBar } from "antd-mobile";
 import "./index.scss";
+import PropTypes from "prop-types";
 
+NavHeader.propTypes = {
+  children: PropTypes.string.isRequired,
+  onLeftClick: PropTypes.func,
+};
 export default function NavHeader({ children, onLeftClick }) {
   const navigate = useNavigate();
   const onBack = () => {
