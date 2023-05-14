e4f8f0efb7956bcd4521d190e5f8b35aff3d512c
hanxiaojian <1095906242@qq.com>
2023-05-14 23:10:49 +0800

    百度地图控件（getscript引入改成api，但是会有警告）

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,11 +1,40 @@
 import React from "react";
 import NavHeader from "../../components/NavHeader";
 import styles from "./index.module.css";
+import { getCurrentCity } from "../../utils";
+const BMapGL = window.BMapGL;
 export default class Map extends React.Component {
-  componentDidMount() {
-    const map = new window.BMapGL.Map("container");
-    const point = new window.BMapGL.Point(116.404, 39.915);
-    map.centerAndZoom(point, 15);
+  async componentDidMount() {
+    const { label, value } = await getCurrentCity();
+    //创建地址解析器实例
+    var myGeo = new BMapGL.Geocoder();
+    // 将地址解析结果显示在地图上，并调整地图视野
+    myGeo.getPoint(
+      label,
+      (point) => {
+        if (point) {
+          const map = new BMapGL.Map("container");
+          map.centerAndZoom(point, 11);
+          // map.addOverlay(new BMapGL.Marker(point, { title: label }));
+
+          //控件
+          map.addControl(
+            new BMapGL.ScaleControl({
+              // offset: new BMapGL.Size(150, 150),
+            })
+          ); // 添加比例尺控件
+          map.addControl(
+            new BMapGL.ZoomControl({
+              // offset: new BMapGL.Size(150, 150),
+            })
+          ); // 添加缩放控件
+          // map.addControl(new BMapGL.CityListControl()); // 添加城市列表控件
+        } else {
+          alert("您选择的地址没有解析到结果！");
+        }
+      },
+      label
+    );
   }
 
   render() {
