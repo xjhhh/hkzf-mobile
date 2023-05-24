75ef1d3c09d36b19473b12a6260bcce6748f8aed
hanxiaojian <1095906242@qq.com>
2023-05-24 22:33:20 +0800

    小区覆盖物

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -70,35 +70,45 @@ export default class Map extends React.Component {
       value,
     } = area;
     const areaPoint = new BMapGL.Point(longitude, latitude);
-    if (type == "rect") {
+    let mapSize, labelContent, labelClick;
+    const map = this.map;
+    if (map.getZoom() == 15) {
+      mapSize = new BMapGL.Size(-50, -28);
+      labelContent = `
+      <div class="${styles.rect}">
+        <span class="${styles.housename}">${areaName}</span>
+        <span class="${styles.housenum}">${count}套</span>
+        <i class="${styles.arrow}"></i>
+      </div>`;
     } else {
-      const label = new BMapGL.Label("", {
-        position: areaPoint, // 指定文本标注所在的地理位置
-        offset: new BMapGL.Size(-35, -35), // 设置文本偏移量
-      });
-      // label.id = value;
-      label.setContent(`
+      mapSize = new BMapGL.Size(-35, -35);
+      labelContent = `
       <div class="${styles.bubble}">
         <p class="${styles.name}">${areaName}</p>
         <p>${count}套</p>
-      </div>`); // 设置房源覆盖物内容
-      label.setStyle(labelStyle); // 设置（文本标注对象）样式
-      const map = this.map;
-      label.addEventListener("click", () => {
+      </div>`;
+      labelClick = () => {
         map.clearOverlays();
-        map.centerAndZoom(areaPoint, zoom);
+        map.centerAndZoom(areaPoint, Math.min(map.getZoom() + 2, 15));
         this.renderOverlays(value);
-      });
-      map.addOverlay(label);
+      };
     }
+    const label = new BMapGL.Label("", {
+      position: areaPoint, // 指定文本标注所在的地理位置
+      offset: mapSize, // 设置文本偏移量
+    });
+    // label.id = value;
+    label.setContent(labelContent); // 设置房源覆盖物内容
+    label.setStyle(labelStyle); // 设置（文本标注对象）样式
+    label.addEventListener("click", labelClick);
+    map.addOverlay(label);
   }
 
   getTypeAndRoom() {
     const zoom = this.map.getZoom(); //区11，镇13，小区15
     const type = {
       11: "circle",
-      13: "circle",
-      15: "rect",
+      13: "rect",
     }[zoom];
     const nextZoom = Math.min(zoom + 2, 15);
     return { type, nextZoom };
